0581982093541f1739cde8acac82141ed02e6747
Siraphob Kanjanasaensong <copsterr@gmail.com>
2020-03-30 00:34:26 +0700

    add controller to test_car.py

--- CARLA_0.9.8/WindowsNoEditor/PythonAPI/examples/test_car.py
+++ CARLA_0.9.8/WindowsNoEditor/PythonAPI/examples/test_car.py
@@ -30,13 +30,13 @@ joystick = pygame.joystick.Joystick(0)  # get joystick
 joystick.init()                         # init joystick
 controller = DS2_Controller(joystick)
 
-
-
 """ CONSTANTS """
 IM_WIDTH = 640
 IM_HEIGHT = 480
 
+
 """ FNS """
+
 def process_img(image):
     i = np.array(image.raw_data)
     i2 = i.reshape((IM_HEIGHT, IM_WIDTH, 4))
@@ -45,16 +45,29 @@ def process_img(image):
     cv2.waitKey(1)
     return i3 / 255.0
 
-
+# list to store actor in the simulation
 actor_list = []
+car_control = carla.VehicleControl()
 
 try:
     # connect to carla server
     client = carla.Client("localhost", 2000)
     client.set_timeout(5.0)
+    print(f"Connected to server.")
 
-    # get world and blueprints
+    # get world
     world = client.get_world()
+
+    """ Somehow commenting this line prevents program from crashing and I
+        don't know why... (crying) """
+    # config settings and set mode to Synchronous with 0.1 time-step
+    # settings = world.get_settings()
+    # settings.fixed_delta_seconds = 0.1
+    # settings.synchronous_mode = True
+    # world.apply_settings(settings)
+    # print("Time-Step/Sync-Mode configured.")
+
+    # load blueprints
     bp_lib = world.get_blueprint_library()
 
     # set weather
@@ -63,6 +76,7 @@ try:
         precipitation=0.0,
         sun_altitude_angle=90.0)
     world.set_weather(weather)
+    print(f"Weather set.")
 
     # get car blueprint
     car_bp = bp_lib.find("vehicle.tesla.cybertruck")
@@ -70,55 +84,82 @@ try:
 
     # spawn a car
     vehicle = world.try_spawn_actor(car_bp, spawn_point)
+    assert vehicle is not None, "Vehicle cannot be spawned!"
+    print(f"Cybertruck spawned.")
     actor_list.append(vehicle)
 
-
-
-    # get rgb camera blueprint
+    # get rgb camera blueprint and set it's attributes
     camera_bp = bp_lib.find("sensor.camera.rgb")
     camera_bp.set_attribute("image_size_x", f"{IM_WIDTH}")
     camera_bp.set_attribute("image_size_y", f"{IM_HEIGHT}")
     camera_bp.set_attribute("fov", "110")
-    camera_bp.set_attribute("sensor_tick", "0.3")
+    camera_bp.set_attribute("sensor_tick", "0.2")  # capture image every 0.3 seconds
 
     # adjust sensor location relative to vehicle
     camera_spawn_point = carla.Transform(carla.Location(x=4.0, z=1.5), carla.Rotation(pitch=-15.0))
 
     # spawn camera
     sensor = world.try_spawn_actor(camera_bp, camera_spawn_point, attach_to=vehicle)
+    assert sensor is not None, "Camera cannot be spawned!"
+    print(f"Camera spawned.")
     actor_list.append(sensor)
 
+    # add image to queue when captured
     image_queue = queue.Queue()
     sensor.listen(image_queue.put)
 
-    # move the car
-    # vehicle.apply_control(carla.VehicleControl(throttle=0.2, steer=0.0))
-
-    done = False
+    GameDone = False  # flag to stop this client (press Ctrl+C to stop)
 
     # GAME HERE
-    while not done:
-        world.tick()
+    while not GameDone:
+        world.tick()  # signal the server to update
+
         tstart = time.time()
 
+        # read events when user do something
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                done = True
+                GameDone = True
 
+        # update controller
         controller.update()
-        if controller.cross:
-            vehicle.apply_control(carla.VehicleControl(throttle=0.2, brake=0))
-            print("Accelerating...")
+        # controller.debug()
 
-        if controller.square:
-            vehicle.apply_control(carla.VehicleControl(throttle=0.0, brake=1.0))
-            print("Brake...")
+        # throttle and brake
+        if controller.analog_left_y <= -0.2:
+            car_control.throttle = -controller.analog_left_y*0.3
+            car_control.brake = 0
+        elif controller.analog_left_y >= 0.2:
+            car_control.brake = controller.analog_left_y*0.3
+            car_control.throttle = 0
+        else:
+            car_control.throttle = 0
 
+        # steering
+        if controller.analog_right_x >= 0.1 or controller.analog_right_x <= -0.1:
+            car_control.steer = controller.analog_right_x*0.3
+        else:
+            car_control.steer = 0
+
+        # hand_brake
+        if controller.L[0]:
+            car_control.hand_brake = True
+        else:
+            car_control.hand_brake = False
+
+        # reverse
+        if controller.R[0]:
+            car_control.reverse = not car_control.reverse
+
+        # apply control to vehicle
+        vehicle.apply_control(car_control)
+
+        # get image from the queue and process
         img = image_queue.get()
         process_img(img)
 
         tprocess = time.time() - tstart
-        # print(f"fps: {round(1/tprocess, 2)}")
+        # print(f"FPS: {round(1/tprocess, 2)}")
 
 
 finally:
